70ac1e7fcae8af11f02679a5f3649fe64b9535f1
Marcelo Santoro <marcelo@iMac-de-Marcelo.local>
2020-04-13 16:45:23 -0300

    BuFix na index.js

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-// var express = require("express");
-// var app = express();
-
-// app.listen(3000, () => {
-//  console.log("Server running on port 3000");
-
-//     app.get("/", (req, res, next) => {
-//         res.json(["Abacaxi","Banana","Maça","Figo","Uva"]);
-//     });
-
-// });
-
-
+// BugFix
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
