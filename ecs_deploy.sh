48bfe9321f8975583635bb2341f59daa61c50ebe
Marcelo Santoro <marcelo@iMac-de-Marcelo.local>
2020-04-16 10:42:55 -0300

    Testing Auto Deploy to AWS ECS Fargate - try 2

--- ecs_deploy.sh
+++ ecs_deploy.sh
@@ -4,6 +4,6 @@
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ] ; then
         AWS_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${EB_REGION}.amazonaws.com
-        docker-deploy/ecs.sh -c $LIVE_CLUSTER_NAME -n $LIVE_SERVICE -i "$AWS_URL/$DOCKER_REPO:latest" -r ${EB_REGION} -t 240
+        bash ecs.sh -c $LIVE_CLUSTER_NAME -n $LIVE_SERVICE -i "$AWS_URL/$DOCKER_REPO:latest" -r ${EB_REGION} -t 240
     fi
 fi
